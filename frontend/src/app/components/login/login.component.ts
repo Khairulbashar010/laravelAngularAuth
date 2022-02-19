32baa24fd7c2123225fd13242189491982892713
Khairul <khairul.bashar.rpsu@gmail.com>
2022-02-19 20:42:19 +0600

    connect angular and laravel

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  HttpClient
+} from '@angular/common/http';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+	public form = {
+		email: null,
+		password: null,
+	};
+	public error = null;
+	constructor(private http: HttpClient) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	onSubmit() {
+		return this.http.post('http://localhost:8000/api/auth/login', this.form).subscribe(
+			data => console.log(data),
+			error => this.handleError(error),
+		);
+	}
+	handleError(error:any) {
+		this.error = error.error.error
+	}
+	ngOnInit(): void {}
 }
